f7a2f967da95e1dd3a9e9563fb2683a65dc2826c
KamelotDeveloper <kamelotdeveloper@github.com>
2026-06-03 10:25:50 -0300

    fix: remove base, set site to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://kamelotdeveloper.github.io',
-  base: '/alma-srl-ingenieria',
+  site: 'https://almaingenieria.com.ar',
   devToolbar: {
     enabled: false,
   },
